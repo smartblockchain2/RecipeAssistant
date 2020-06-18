9242e5b53b25480114f9c82e9d7b68c964b343d4
smartblockchain2 <smartblockchain2@gmail.com>
2020-06-18 09:57:02 -0500

    Updates

---
+++ precache-manifest.7c9ebcc178f470cf1bb425ff2e93cf1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "701518fdc6b2ada3f6edd421f9f8bcd9",
+    "url": "/RecipeAssistant/index.html"
+  },
+  {
+    "revision": "10d94310a9aa23e55719",
+    "url": "/RecipeAssistant/static/css/main.d4f8dc25.chunk.css"
+  },
+  {
+    "revision": "8b0b824019a2e8cbe1fb",
+    "url": "/RecipeAssistant/static/js/2.f8164c85.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RecipeAssistant/static/js/2.f8164c85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10d94310a9aa23e55719",
+    "url": "/RecipeAssistant/static/js/main.38fdb723.chunk.js"
+  },
+  {
+    "revision": "1c0364f827870f53be83",
+    "url": "/RecipeAssistant/static/js/runtime-main.1978d8ef.js"
+  }
+]);
